7c4facb17f0e3b15996e467262129df0cf1928f4
alicia-colom <aliciacolom@gmail.com>
2021-02-16 13:05:31 +0100

    Initial commit

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const QuestionCard = () => {
+	return <div>Question card</div>;
+};
+
+export default QuestionCard;
---
+++ src/utils.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const utils = () => {
+	return <div></div>;
+};
+
+export default utils;
