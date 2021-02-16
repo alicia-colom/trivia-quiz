6752f5c99f22b10413fab003b0792122d6a1d788
alicia-colom <aliciacolom@gmail.com>
2021-02-16 13:25:11 +0100

    Create App component structure

--- src/API.tsx
+++ src/API.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const API = () => {
-	return <div></div>;
+	return (
+		<div>
+			URL de la API: https://opentdb.com/api.php?amount=10“&amp;”type=multiple
+		</div>
+	);
 };
 
 export default API;
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import QuestionCard from './components/QuestionCard';
 
 function App() {
+	const startTrivia = async () => {};
 
-const startTrivia = async () => {};
+	const checkAnswer = (ev: React.MouseEvent<HTMLButtonElement>) => {};
 
-const checkAnswer = (ev: React.MouseEvent<HTMLButtonElement>) => {};
+	const nextQuestion = () => {};
 
-return <div>Take the Quiz</div>;
+	return (
+		<>
+			<h1>Take the Quiz</h1>
+			<button className="start" onClick={startTrivia}>
+				Start quiz
+			</button>
+			<p className="score">Your score:</p>
+			<p className="loadingQuestion">Loading questions...</p>
+			<QuestionCard />
+			<button className="next" onClick={nextQuestion}>
+				Go to next question
+			</button>
+		</>
+	);
 }
 
 export default App;
